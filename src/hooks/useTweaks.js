2723cda41cb12ae77205f6e1e327b2b1e040781a
강동규 <kangkg0067@gmail.com>
2026-05-21 17:41:23 +0900

    chore: 루트 중복 프론트엔드 파일 제거

--- src/hooks/useTweaks.js
+++
@@ -1,9 +0,0 @@
-import { useState, useCallback } from 'react'
-
-export function useTweaks(defaults) {
-  const [values, setValues] = useState(defaults)
-  const setTweak = useCallback((key, val) => {
-    setValues(prev => ({ ...prev, [key]: val }))
-  }, [])
-  return [values, setTweak]
-}
